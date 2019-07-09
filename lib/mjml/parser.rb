819b29c717a56a4f3479e81579383e0fd4d730a4
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2019-07-09 16:53:20 +0900

    Close pipe

--- lib/mjml/parser.rb
+++ lib/mjml/parser.rb
@@ -36,8 +36,9 @@ module Mjml
     def run(in_tmp_file, beautify=true, minify=false)
       Tempfile.create(["out", ".html"]) do |out_tmp_file|
         command = "#{mjml_bin} -r #{in_tmp_file} -o #{out_tmp_file.path} --config.beautify #{beautify} --config.minify #{minify}"
-        _, _, stderr, _ = Open3.popen3(command)
-        raise ParseError.new(stderr.read.chomp) unless stderr.eof?
+        Open3.popen3(command) do |_, _, stderr, _|
+          raise ParseError.new(stderr.read.chomp) unless stderr.eof?
+        end
         out_tmp_file.read
       end
     end
